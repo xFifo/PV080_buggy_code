69d575d06826875dedfd81608283866a99eff5b8
xFifo <564390@mail.muni.cz>
2026-05-05 14:57:05 +0200

    Update buggy_python_code.py

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -28,7 +28,7 @@ def fetch_website(urllib_version, url):
     exec(f"import urllib{urllib_version} as urllib", globals())
     # Fetch and print the requested URL
 
-    try: 
+    try:
         http = urllib.PoolManager()
         r = http.request('GET', url)
     except:
@@ -64,4 +64,3 @@ if __name__ == '__main__':
     elif choice == "4":
         password = input("Enter master password: ")
         authenticate(password)
-
